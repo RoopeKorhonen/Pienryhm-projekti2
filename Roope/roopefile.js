a2ddcba2fa093a36810d2f17ecf47530b6f2a932
roope <roope.korhonen2@metropolia>
2022-12-02 13:12:24 +0200

    Roopen 2 päivän push

--- Roope/roopefile.js
+++ Roope/roopefile.js
@@ -1,36 +1,36 @@
-'use strict';
-console.log("Program starts")
-const hs_button = document.getElementById('highscore');
-hs_button.addEventListener('click', async function(event){
-    // estää refresh <form>issa
-    event.preventDefault()
-    console.log('click event', event)
-    let highscore_list = highscore()
-    console.log('highscore list', highscore_list)
-})
-async function play() {
-    try {
-        const response = await fetch('http://127.0.0.1:5000/play/');
-        const data = await response.json();
-        console.log('result', data);
-        //renderResult(data);
-        return data
-    } catch (error) {
-        console.log('Verkkovirhe: ', error)
-    }
-}
-async function highscore(){
-    try{
-        const response = await fetch('http://127.0.0.1:5000/get_highscore/');
-        const data = await response.json();
-        console.log('result', data);
-        //renderResult(data);
-        return data
-        } catch (error){
-        console.log('Verkkovirhe: ', error)
-    }
-}
-}
+//*'use strict';
+// console.log("Program starts")
+// const hs_button = document.getElementById('highscore');
+// hs_button.addEventListener('click', async function(event){
+//     // estää refresh <form>issa
+//     event.preventDefault()
+//     console.log('click event', event)
+//     let highscore_list = highscore()
+//     console.log('highscore list', highscore_list)
+// })
+// async function play() {
+//     try {
+//         const response = await fetch('http://127.0.0.1:5000/play/');
+//         const data = await response.json();
+//         console.log('result', data);
+//         //renderResult(data);
+//         return data
+//     } catch (error) {
+//         console.log('Verkkovirhe: ', error)
+//     }
+// }
+// async function highscore(){
+//     try{
+//         const response = await fetch('http://127.0.0.1:5000/get_highscore/');
+//         const data = await response.json();
+//         console.log('result', data);
+//         //renderResult(data);
+//         return data
+//         } catch (error){
+//         console.log('Verkkovirhe: ', error)
+//     }
+// }
+// }
 //*async function getAirport(icao){
 // try{
 //     const response = await fetch('http://127.0.0.1:5000/airport/' + icao);
--- Roope/roopehighscore_menu.js
+++ Roope/roopehighscore_menu.js
@@ -1,22 +1,21 @@
 'use strict';
 console.log("Program starts")
-const hs_button = document.querySelector('button');
-hs_button.addEventListener('click', async function(event){
-    // estää refresh <form>issa
-    event.preventDefault()
-    console.log('click event', event)
-    let highscore_list = highscore()
-    console.log('highscore list', highscore_list)
-
-
-async function highscore(){
-    try{
+async function highscore() {
+    try {
         const response = await fetch('http://127.0.0.1:5000/get_highscore/');
         const data = await response.json();
         console.log('result', data);
         //renderResult(data);
         return data;
-        } catch (error){
+    } catch (error) {
         console.log('Verkkovirhe: ', error)
     }
-    }
+}
+const hs_button = document.querySelector('button');
+hs_button.addEventListener('click', async function(event){
+    // estää refresh <form>issa
+    event.preventDefault()
+    console.log('click event', event)
+    let highscores_list = highscore()
+    console.log('highscore list', highscores_list)
+})
