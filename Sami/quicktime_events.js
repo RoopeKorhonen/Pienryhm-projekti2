ee9cdd7a295d197749c48f5abe8393c328fdc4f1
samwise50000 <sami.mattila3@metropolia.fi>
2022-12-10 12:20:50 +0200

    Quicktime eventit done.

--- Sami/quicktime_events.js
+++ Sami/quicktime_events.js
@@ -74,70 +74,62 @@ const questions = [
 ];
 
 
-let right_answer = ''
-let wrong_answer = ''
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-let correct = ''
-
 //Kun lento tapahtuu, niin tulee kutsu tähän funktioon
-//arpominen 1/3 mahdollisuudellta funktio quicktime toteutuisi vielä tekemättä.
+
 function quicktime() {
-    let question_num = Math.floor(Math.random() * questions.length);
-    let header = document.querySelector('h1');
-    let question_text = questions[question_num].question;
-    right_answer = questions[question_num].correct_answer
-    wrong_answer = questions[question_num].wrong_answer
-    correct = right_answer
-    header.innerHTML = question_text
-    let button_rand = Math.floor(Math.random() * 2)
-    if (button_rand === 1){
-        button1.innerText = right_answer
-        button2.innerText = wrong_answer
-    }else{
-        button1.innerText = wrong_answer
-        button2.innerText = right_answer
+    let right_answer = ''
+    let wrong_answer = ''
+    const button1 = document.getElementById('button1');
+    const button2 = document.getElementById('button2');
+    let correct = ''
+    if (Math.random() < 1 / 3) {
+        let question_num = Math.floor(Math.random() * questions.length);
+        let header = document.querySelector('h1');
+        let question_text = questions[question_num].question;
+        right_answer = questions[question_num].correct_answer
+        wrong_answer = questions[question_num].wrong_answer
+        correct = right_answer
+        header.innerHTML = question_text
+        let button_rand = Math.floor(Math.random() * 2)
+        if (button_rand === 1) {
+            button1.innerText = right_answer
+            button2.innerText = wrong_answer
+        } else {
+            button1.innerText = wrong_answer
+            button2.innerText = right_answer
+
+        }
+        console.log(question_text)
+        button1.addEventListener('click', function () {
+            let answer = button1.innerText;
+
+            if (answer === correct) {
+                // lisää 100 co2 budjettiin
+
+                console.log("OIKEIN!!!!!!!!!!!!!!")
+            } else {
+                console.log("VÄÄRIN!!!!!!!!!!!!!!!!!!!!!!")
+            }
+
+        })
+
+        button2.addEventListener('click', function () {
+            let answer = button2.innerText;
+
+            if (answer === correct) {
+                // lisää 100 co2 budjettiin
+
+                console.log("OIKEIN!!!!!!!!!!!!!!")
+            } else {
+                console.log("VÄÄRIN!!!!!!!!!!!!!!!!!!!!!!")
+            }
+
+        })
+        return question_text
 
     }
-    console.log(question_text)
-    return question_text
+
 }
-
-button1.addEventListener('click', function (){
-    let answer = button1.innerText;
-
-    if (answer === correct){
-        // lisää 100 co2 budjettiin
-
-        console.log("OIKEIN!!!!!!!!!!!!!!")
-    } else {
-        console.log("VÄÄRIN!!!!!!!!!!!!!!!!!!!!!!")
-    }
-
-})
-
-button2.addEventListener('click', function (){
-    let answer = button2.innerText;
-
-    if (answer === correct){
-        // lisää 100 co2 budjettiin
-
-        console.log("OIKEIN!!!!!!!!!!!!!!")
-    } else {
-        console.log("VÄÄRIN!!!!!!!!!!!!!!!!!!!!!!")
-    }
-
-})
-
-/* function checkAnswer(){
-    if (button1.innerText === right_answer){
-        console.log("CORRECT1!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    } else {
-        console.log("WRONG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-
-    }
-
-} */
 
 
 quicktime()
